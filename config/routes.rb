0eeca99bf4a7184d2f57fb59e95371dbe4da3afb
Jon Widener <jon.widener@gmail.com>
2023-04-07 14:45:14 -0500

    Cleans up routes.rb slightly

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   resources :constituents
   resources :herbs
   get 'dashboard/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "dashboard#index"
 end
