e59eae6f008460d1455baf66687f7f9e97c1f2cb
Jon Widener <jon.widener@gmail.com>
2023-04-11 20:50:25 -0500

    Adds slightly better navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :constituents
   resources :herbs
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "dashboard/index"
+
+  root "dashboard#index"
 end
